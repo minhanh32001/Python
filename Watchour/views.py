b7d30cd217169fa744c7f75503dac4e741c7b48f
Win Lã <dangcapcuawin@gmail.com>
2023-06-07 13:53:13 +0700

    update UI ...not yet

--- Watchour/views.py
+++ Watchour/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from store.models import Product
+from category.models import Category
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
+    categories = Category.objects.all()[:7]
+    selected_category = request.GET.get('category_name', '')
+
+    if selected_category:
+        selected_category = Category.objects.get(category_name=selected_category)
+        products = Product.objects.filter(category=selected_category)
+    else:
+        products = Product.objects.all()
     context = {
         'products': products,
+        'categories': categories,
+        'selected_category': selected_category,
     }
     return render(request, 'home.html', context=context)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./templates/**/*.html",
-    "./node_modules/flowbite/**/*.js",
-    "node_modules/preline/dist/*.js",
-  ],
-  theme: {
-    colors: {
-      "primary-color": "#f7ab0a",
-      "primary-hover-color": "#F7CC0A",
-      "gray-color": "#242424",
-      "gray-hover-color": "#4F4F4F",
-    },
-    extend: {},
-  },
-  plugins: [require("flowbite/plugin"), require("preline/plugin"), require("daisyui")],
+	darkMode: "class",
+	content: [
+		"./templates/**/*.html",
+		"./node_modules/flowbite/**/*.js",
+		"node_modules/preline/dist/*.js",
+	],
+	theme: {
+		colors: {
+			"primary-color": "#f7ab0a",
+			"primary-hover-color": "#F7CC0A",
+			"gray-color": "#242424",
+			"gray-hover-color": "#4F4F4F",
+		},
+		extend: {
+			gridTemplateRows: {
+				"[auto,auto,1fr]": "auto auto 1fr",
+			},
+		},
+	},
+	plugins: [
+		require("@tailwindcss/aspect-ratio"),
+		require("@tailwindcss/forms"),
+		require("flowbite/plugin"),
+		require("preline/plugin"),
+		require("daisyui"),
+	],
 };

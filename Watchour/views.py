3c6672b3cceea89af59ac4f29cbccaa49f1fdf1f
Win Lã <dangcapcuawin@gmail.com>
2023-06-16 09:11:13 +0700

    frontend update

--- Watchour/views.py
+++ Watchour/views.py
@@ -5,16 +5,10 @@ from category.models import Category
 
 def home(request):
     categories = Category.objects.all()[:7]
-    selected_category = request.GET.get('category_name', '')
 
-    if selected_category:
-        selected_category = Category.objects.get(category_name=selected_category)
-        products = Product.objects.filter(category=selected_category)
-    else:
-        products = Product.objects.all()
+    products = Product.objects.all()
     context = {
         'products': products,
         'categories': categories,
-        'selected_category': selected_category,
     }
     return render(request, 'home.html', context=context)
